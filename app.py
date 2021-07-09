5da811fb93fc3e3ab3e339ce3887c8f4d90d6b47
Sahil <sahil280114@gmail.com>
2021-07-09 12:35:44 +0200

    shift to prod settings

--- app.py
+++ app.py
@@ -4,19 +4,28 @@ import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
+
+st.set_page_config(page_title='Auto EDA', layout = 'wide', initial_sidebar_state = 'auto')
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # Web App Title
 st.markdown('''
-# **The EDA App**
-This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+# **Auto-EDA App**
+Generate commonly used descriptive statistics and plots from a csv file. Built using streamlit and pandas_profiling
+
 ---
 ''')
 
 # Upload CSV data
-with st.sidebar.header('1. Upload your CSV data'):
+with st.sidebar.header('Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-""")
+    
 
 # Pandas Profiling Report
 if uploaded_file is not None:
